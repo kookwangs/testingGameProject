7b27786e5b62fd39f51ddf06b4fa8ba7ccb96482
kookwangs <82722851+kookwangs@users.noreply.github.com>
2022-10-06 19:21:54 +0700

    game correct until ending hook

--- game.py
+++ game.py
@@ -30,32 +30,34 @@ scoreValue = 0
 
 def dogBeat(n,t):
         scoreValue=0
-        if presentTicks>=t and presentTicks<=t+0.5 and a[n]==0 :
+        if presentTicks>=t and presentTicks<=t+1 and a[n]==0 :
                 if keys[pygame.K_LEFT]:
                         scoreValue+=50
                         hitSound.play()
                         a[n]=1
+                        pygame.time.delay(100)
                 elif keys[pygame.K_RIGHT] :
                         missSound.play()
                         a[n]=1
-        elif presentTicks>t+0.5 and a[n]==0:
+        elif presentTicks>t+1 and a[n]==0:
                 missSound.play()
                 a[n]=1
-        elif a[n]==0 and presentTicks<=t-0.5 and (keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]):
+        elif a[n]==0 and presentTicks<=t-1 and (keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]):
                 missSound.play()
         return scoreValue
 
 def catBeat(n,t):
         scoreValue=0
-        if presentTicks>=t and presentTicks<=t+0.5 and a[n]==0 :
+        if presentTicks>=t and presentTicks<=t+1 and a[n]==0 :
                 if keys[pygame.K_RIGHT]:
                         scoreValue+=50
                         hitSound.play()
                         a[n]=1
+                        pygame.time.delay(100)
                 elif keys[pygame.K_LEFT] :
                         missSound.play()
                         a[n]=1
-        elif presentTicks>t+0.5 and a[n]==0:
+        elif presentTicks>t+1 and a[n]==0:
                 missSound.play()
                 a[n]=1
         elif a[n]==0 and a[n-1]==1 and (keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]):
@@ -103,31 +105,53 @@ while True:
         #For checking time
 
         if a[0]==0:
-                scoreValue+=dogBeat(0,4.05)
+                scoreValue+=dogBeat(0,4.00)
         elif a[0]==1 and a[1]==0:
                 scoreValue+=catBeat(1,9.00)
         elif a[1]==1 and a[2]==0:
                 scoreValue+=dogBeat(2,11.00)
         elif a[2]==1 and a[3]==0:
-                scoreValue+=dogBeat(3,13.50)
+                scoreValue+=dogBeat(3,13.60)
         elif a[3]==1 and a[4]==0:
-                scoreValue+=catBeat(4,15.00)
+                scoreValue+=catBeat(4,15.60)
         elif a[4]==1 and a[5]==0:
-                scoreValue+=dogBeat(5,17.50)
-                
-                
+                scoreValue+=dogBeat(5,17.90)
+        elif a[5]==1 and a[6]==0:
+                scoreValue+=dogBeat(6,22.60)
+        elif a[6]==1 and a[7]==0:
+                scoreValue+=catBeat(7,25.30)
+        elif a[7]==1 and a[8]==0:
+                scoreValue+=dogBeat(8,28.60)
+        elif a[8]==1 and a[9]==0:
+                scoreValue+=dogBeat(9,29.60)
+        elif a[9]==1 and a[10]==0:
+                scoreValue+=catBeat(10,30.60)
+        elif a[10]==1 and a[11]==0:
+                scoreValue+=dogBeat(11,32.10)
+        elif a[11]==1 and a[12]==0:
+                scoreValue+=catBeat(12,37.80)
+        elif a[12]==1 and a[13]==0:
+                scoreValue+=dogBeat(13,39.00)
+        elif a[13]==1 and a[14]==0:
+                scoreValue+=catBeat(14,41.50)
+        elif a[14]==1 and a[15]==0:
+                scoreValue+=dogBeat(15,46.00)
+        elif a[15]==1 and a[16]==0:
+                scoreValue+=dogBeat(16,48.00)
+        elif a[16]==1 and a[17]==0:
+                scoreValue+=dogBeat(17,49.00)
+        elif a[17]==1 and a[18]==0:
+                scoreValue+=catBeat(18,51.50)
         
-                
-                
-                 
         
-                
+        
+   
                         
         screen.blit(bg,(0,0))
         screen.blit(player,(posX,posY))
         showScore()
         testText()
-       
+        pygame.time.delay(10)
         posX=375
         posY=310
         pygame.display.update()
